73ce13aaaf97eb606641a3dfbbc352cb01e72fd9
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-10-18 00:07:49 +0530

    Update instructor.java

--- instructor.java
+++ instructor.java
@@ -1,12 +1,23 @@
 import java.util.*;
+import java.lang.*;
 
 public class instructor implements User{
 
     private String curr_ins;
+    private ArrayList<student> stud = new ArrayList<>();
 
-    public instructor(String ins)
+    private Scanner sc = new Scanner(System.in);
+    private Scanner scan = new Scanner(System.in); 
+
+    public static lecture lec = new lecture();
+    public static assessment as = new assessment();
+    
+    public static HashMap<String,HashMap> comment = new HashMap<>();
+
+    public instructor(String ins, ArrayList stud)
     {
         this.curr_ins = ins;        
+        this.stud = stud;
     }
 
     @Override
@@ -16,11 +27,11 @@ public class instructor implements User{
     }
 
     @Override
-    public void menu(){
+    public void menu(int opt)
+    {
         
-        Scanner sc = new Scanner(System.in);
         Program pg = new Program();
-
+    
         System.out.print("\nINSTRUCTOR MENU\n");
         System.out.print("1. Add class material \n");
         System.out.print("2. Add assessments \n");
@@ -34,20 +45,136 @@ public class instructor implements User{
 
         int cho = sc.nextInt();
 
-        if(cho == 9)
+        if(cho == 1)
+        {
+            System.out.print("\n1. Add Lecture Slide");
+            System.out.print("\n2. Add Lecture Video\n");
+            int c = sc.nextInt();
+
+            if(c == 1)
+            {
+                lec.addslide(opt);
+                menu(opt);
+            } 
+
+            else if(c == 2)
+            {
+                lec.addvideo(opt);
+                menu(opt);
+            }
+
+            else
+            {
+                System.out.println("Wrong option");
+                System.exit(0);
+            }
+        }
+        
+        else if(cho == 2)
+        {
+            System.out.println("1. Add Assignment");
+            System.out.println("2. Add Quiz");
+            int c = sc.nextInt();
+
+            if(c == 1)
+            {
+                as.add_assignment(opt);
+                menu(opt);
+            }
+
+            else if(c == 2)
+            {
+                as.add_quiz(opt);
+                menu(opt);
+            }
+            
+            else
+            {
+                System.out.println("Wrong option");
+                System.exit(0);
+            }
+        }
+
+        else if(cho == 3)
+        {
+            view_material(opt);
+        }
+
+        else if(cho == 4)
+        {
+            view_assessments(opt);
+        }
+
+        else if(cho == 7)
+        {
+            view_comments();
+            menu(opt);
+        }
+
+        else if(cho == 8)
+        {
+            add_comments(opt);
+            menu(opt);
+        }
+
+        else if(cho == 9)
         {
             Main.main_menu(pg);
         }
     }
 
     @Override
-    public void view_comments(){
-        //aaa
+    public void view_material(int opt)
+    {
+        instructor.lec.viewslide();
+        instructor.lec.viewvideo();
+        System.out.println("Welcome I" + opt);
+        menu(opt);
     }
 
     @Override
-    public void add_comments(){
-        //aaa
+    public void view_assessments(int opt)
+    {
+        as.view_all();
+        System.out.println("Welcome I" + opt);
+        menu(opt);
+    }
+
+    @Override
+    public void view_comments()
+    {
+        for(HashMap.Entry<String,HashMap> trav : comment.entrySet())
+        {
+            HashMap<String,Date> travmap = new HashMap<>();
+
+            travmap = trav.getValue();
+
+            for(HashMap.Entry<String,Date> ite : travmap.entrySet())
+            {
+                Date x = new Date();
+                x = ite.getValue();
+    
+                System.out.println(trav.getKey() + " - " + ite.getKey());
+                System.out.println(x);
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    public void add_comments(int opt)
+    {
+        System.out.print("\nEnter comment: ");
+        String com = scan.nextLine();
+        String stf = "I" + opt;
+
+        HashMap<String,Date> co = new HashMap<>();
+        
+        long millis=System.currentTimeMillis(); 
+        java.util.Date date=new java.util.Date(millis);
+
+        co.put(stf, date);
+        comment.put(com, co);
     }
     
 }
