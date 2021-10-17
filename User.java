4a5a98505930a4650778017c47883d952ae283da
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-10-18 00:08:57 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
 public interface User{
-    public void menu();
+    public void menu(int opt);
     public void view_comments();
-    public void add_comments();
+    public void add_comments(int opt);
     public String current();
+    public void view_material(int opt);
+    public void view_assessments(int opt);
 }
 
 
